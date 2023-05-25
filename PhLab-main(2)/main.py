3f006550ec7fd5d998c26af98ff8f80ae582338f
poi000op <127398215+poi000op@users.noreply.github.com>
2023-05-25 21:57:45 +0300

    main.py

--- PhLab-main(2)/main.py
+++ PhLab-main(2)/main.py
@@ -1,8 +1,4 @@
-
-import pymunk.pygame_util
-pymunk.pygame_util.positive_y_is_up = False
-
-from Constants import *
+from Space import space
 from Drawing import DrawThePicture
 
 DrawThePicture(space)
