d753bc94217a619f9ed2e9d83909b2f1c4e760db
poi000op <127398215+poi000op@users.noreply.github.com>
2023-05-25 21:56:13 +0300

    Add files via upload

--- PhLab-main(2)/Constants.py
+++ PhLab-main(2)/Constants.py
@@ -1,11 +1,15 @@
 import pymunk
+from Space import space
 import pygame as pg
 import pymunk.pygame_util
+from Unpacing import Unpacking_Modul
+b = Unpacking_Modul()
+b = b.Option()
 
 # Настройки экрана и фона. Параметры для отрисовки.
-
-RES = WIDTH, HEIGHT = 900, 720
-FPS = 300
+RES = WIDTH, HEIGHT = b["WIDTH"], b["HEIGHT"]
+FPS = b["FPS"]
+Backgroud_Color = (b["Backgroud_Color_R"], b["Backgroud_Color_G"], b["Backgroud_Color_B"])
 
 pg.init()
 surface = pg.display.set_mode(RES)
@@ -14,8 +18,7 @@ draw_options = pymunk.pygame_util.DrawOptions(surface)
 
 # Параметры физического пространства
 
-space = pymunk.Space()
-space.gravity = 0, 8000
+space.gravity = b["gravity_x"], b["gravity_y"]
 
 
 
