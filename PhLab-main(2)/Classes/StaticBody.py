a4da103144314f6748130c7387c228bd56e5d204
RozovaSofia <127396762+RozovaSofia@users.noreply.github.com>
2023-05-25 22:26:24 +0300

    Add files via upload

---
+++ PhLab-main(2)/Classes/StaticBody.py
@@ -0,0 +1,27 @@
+import pymunk
+from random import randrange
+
+
+class StaticBody:
+    def __init__(self, a):
+        self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        self.pos_x=a["pos_x"]
+        self.pos_y=a["pos_y"]
+
+        if a["type"] == "circle":
+            self.radius = a["radius"]
+            self.shape = pymunk.Circle(self.body, self.radius)
+
+        if a["type"] == "square":
+            self.size_x = a["size_x"]
+            self.size_y = a["size_y"]
+            self.size = (self.size_x, self.size_y)
+            self.shape = pymunk.Poly.create_box(self.body, self.size)
+
+
+    def create (self, space):
+        self.body.position = (self.pos_x,self.pos_y)
+        self.shape.color = [randrange(256) for i in range(4)]
+        space.add(self.body, self.shape)
+
+
