33922e1c18b46fffd338bac2288acfa3281498e1
poi000op <127398215+poi000op@users.noreply.github.com>
2023-05-25 21:56:49 +0300

    Add files via upload

--- PhLab-main(2)/Drawing.py
+++ PhLab-main(2)/Drawing.py
@@ -1,25 +1,22 @@
 import pygame as pg
+from Space import space
 from Constants import *
 from Classes.Platform import Platform
 from Classes.Circle import Circle
 from Classes.Square import Square
+from Classes.Spring import Spring
 from Unpacing import Unpacking_Modul
 # Класс Рисование
 # Рисует и создает объекты в соответствии с нажатием мыши
-a = Unpacking_Modul()
+
 def DrawThePicture (space):
+    a = Unpacking_Modul()
     a.add(space)
     while True:
-        surface.fill(pg.Color('white'))
+        surface.fill(Backgroud_Color)
         for i in pg.event.get():
             if i.type == pg.QUIT:
                 exit()
-
-            # if i.type == pg.MOUSEBUTTONDOWN:
-            #     if i.button == 1:
-            #         C=Square(12,100,100,1,1)
-            #         C.create(i.pos,space)
-
 
         space.step(1 / FPS)
         space.debug_draw(draw_options)
